d64c9a8645e8ea06bd2fe26979bcecdadbf9a2fe
kashyap1ankit <kashyap25ankit@gmail.com>
2024-08-14 12:19:29 +0530

    created the singleton prisma client & wrote the logic for saving google signed-up user in db

--- auth.ts
+++ auth.ts
@@ -2,66 +2,42 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import { signinSchema } from "./schema/signin";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-const prisma = new PrismaClient().$extends(withAccelerate());
+import prisma from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    Credentials({
-      authorize: async (credentials, req: Request) => {
-        let body = await req.json();
-        body.skills = body.skills.split(",");
-        const { success } = signinSchema.safeParse(body);
-        if (!success)
-          return {
-            status: 400,
-            message: "Schema validation failed",
-          };
-
-        const user = await prisma.user.findFirst({
-          where: {
-            username: body.username,
-          },
-          select: {
-            id: true,
-          },
-        });
-        if (!user) {
-          const response = await prisma.user.create({
-            data: {
-              username: body.username,
-              email: body.email,
-              password: body.password,
-              avatar: body.avatar,
-              bio: body.bio,
-              instagram_url: body.instagram_url,
-              linkedin_url: body.linkedin_url,
-              twitter_url: body.twitter_url,
-              skills: body.skills,
+  providers: [Google],
+  callbacks: {
+    async signIn({ user, profile, account, email, credentials }) {
+      if (account?.provider === "google") {
+        try {
+          const userDetials = await prisma.user.findFirst({
+            where: {
+              email: user.email as string,
             },
           });
 
-          console.log("user created", response);
-          return {
-            id: response.id,
-            username: response.username,
-          };
-        }
+          if (!userDetials) {
+            const createdUser = await prisma.user.create({
+              data: {
+                username: user.email as string,
+                email: user.email as string,
+                avatar: user.image,
+                provider: account.provider,
+                provider_id: account.providerAccountId,
+              },
+            });
 
-        return credentials;
-      },
-    }),
-    Google,
-  ],
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      return "/";
+            console.log("user details", createdUser);
+            return true;
+          }
+          console.log("already exits", userDetials);
+          return true;
+        } catch (error) {
+          console.log("error", error);
+          return false;
+        }
+      }
+      return false;
     },
-  },
-  pages: {
-    signIn: "/signin",
   },
 });
---
+++ prisma/migrations/20240814052428_session_added/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" ALTER COLUMN "authorId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
